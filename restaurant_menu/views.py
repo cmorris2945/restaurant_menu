3837445047c6228d47b4e3e761d3eca7fac2cfa8
cmorris <cmorris2945@yahoo.com>
2024-04-11 21:01:25 -0400

    made urls file and moved templates directory

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views import generic
 from . models import Item
@@ -7,6 +8,9 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {"meals": "Pizza"}
+        return context
 
 
 class MenuItemDetail(generic.DetailView):
